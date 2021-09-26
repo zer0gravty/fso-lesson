dfd52161ce2c6f9614dc141da02be280625f3744
Frank Vamos, Jr <fvamos@gmail.com>
2021-09-25 21:39:35 -0400

    Updated build folder

--- server/index.js
+++ server/index.js
@@ -42,9 +42,9 @@ app.use(cors());
 app.use(requestLogger);
 app.use(express.static('build'));
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>');
-});
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello World</h1>');
+// });
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
